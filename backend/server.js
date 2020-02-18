a76c3b57403baa46960d47a4cfd75d295ab935be
Jay Pal <jayp@simplifyworkforce.com>
2020-02-18 21:03:35 +0530

    express api router

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,28 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 var messages = [{ text: "some text", owner: "Tim" }, { text: "other message", owner: "Jane" }];
 
+app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-})
+});
 
-app.get('/api/messages', (req, res) => {
+var api = express.Router();
+
+api.get('/messages', (req, res) => {
     res.json(messages);
 });
 
+api.post('/message', (req, res) => {
+    console.log(req.body);
+    messages.push(req.body);
+    res.sendStatus(200);
+});
 
+app.use('/api', api);
 
 app.listen(1234);
