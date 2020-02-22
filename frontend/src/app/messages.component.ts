265d923d15151574fb8442bb240794d8a3af6b60
Jay Pal <contactjaypal@gmail.com>
2020-02-22 17:23:48 +0530

    retrieve a route params

--- frontend/src/app/messages.component.ts
+++ frontend/src/app/messages.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { WebService } from './web.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-messages',
@@ -14,7 +15,9 @@ import { WebService } from './web.service';
 })
 
 export class MessagesComponent {
-    constructor(private webService: WebService) {
+    constructor(private webService: WebService, private route: ActivatedRoute) {
     }
-
+    ngOnInit() {
+        console.log(this.route.snapshot.params.name);
+    }
 }
